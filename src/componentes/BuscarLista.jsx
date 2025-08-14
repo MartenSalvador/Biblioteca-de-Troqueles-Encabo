c0783dcc37b9efea0e17ad3e8ef278b0881ce91f
Martin Salvador <martinandressalvador@gmail.com>
2025-08-14 22:56:09 +0200

    Modificando Css de la lista de busqueda y haciendo responsiva la app

--- src/componentes/BuscarLista.jsx
+++ src/componentes/BuscarLista.jsx
@@ -8,9 +8,10 @@ function BuscarLista({ troqueles =[] }){
     return <p>No hay resultados.</p>;
   }
     return(
-        <ul>
+        <ul className="ul-lista">
             {troqueles.map(troquel => (
-                <li key={troquel.id}>
+                <li className="li-lista"
+                    key={troquel.id}>
                     {troquel.tipo === "rectangular"
                         ? `${troquel.base}x${troquel.altura}`
                         : `${troquel.baseMayor}x${troquel.baseMenor}x${troquel.altura}`
